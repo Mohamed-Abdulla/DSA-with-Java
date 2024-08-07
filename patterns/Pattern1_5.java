1446f0c869ff46f2ef4c329ed9c143f7c4fd49d7
mohamed-abdulla <abdabdulla78@gmail.com>
2024-08-07 20:01:22 +0530

    5 pattern ques solved

---
+++ patterns/Pattern1_5.java
@@ -0,0 +1,50 @@
+package patterns;
+
+public class Pattern1_5 {
+
+    public static void main(String[] args) {
+        int n=5;
+//        pattern1(n);
+////        pattern2(n);
+//        pattern3(n);
+        pattern4(n);
+
+    }
+
+    static void pattern1(int n){
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    static void pattern2(int n){
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    static void pattern3(int n){
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col < n-row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    static void pattern4(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col < row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
